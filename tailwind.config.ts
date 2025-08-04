21dbc720eda7697c9014506cd01bad1bee0f2596
Muhammad Abdullah <m.abdullahx21@gmail.com>
2025-08-04 22:56:09 +0400

    debug module errors

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+const tailwindConfig = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
